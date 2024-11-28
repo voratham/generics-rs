7079d959602f393b008e02f41b51d1a095e4eb13
voratham.s <voratham.s@linecorp.com>
2024-11-28 23:35:51 +0700

    chore: super solve flexibility trait

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use num_traits::{Float, ToPrimitive};
+use num_traits::ToPrimitive;
 
 // First version - we can pass in BOTH f32 or f64
 //  Second -- we can pass in any type of numbers
@@ -9,7 +9,7 @@ use num_traits::{Float, ToPrimitive};
 //     (a_f64.powi(2) + b_f64.powi(2)).sqrt()
 // }
 
-//  Generic Type - Like an argument list, but for types.
+// NOTE: Generic Type - Like an argument list, but for types.
 // fn solve<T: Float>(a: T, b: T) -> f64 {
 //     let a_f64 = a.to_f64().unwrap();
 //     let b_f64 = b.to_f64().unwrap();
@@ -17,16 +17,26 @@ use num_traits::{Float, ToPrimitive};
 //     (a_f64.powi(2) + b_f64.powi(2)).sqrt()
 // }
 
-// Multiple Generic Type
-fn solve<T: Float, U: Float>(a: T, b: U) -> f64 {
+// NOTE: Multiple Generic Type with import num_trait
+// use num_traits::{ToPrimitive, Float};
+// fn solve<T: Float, U: Float>(a: T, b: U) -> f64 {
+//     let a_f64 = a.to_f64().unwrap();
+//     let b_f64 = b.to_f64().unwrap();
+
+//     (a_f64.powi(2) + b_f64.powi(2)).sqrt()
+// }
+
+// NOTE:  Super solve flexible with ToPrimitive
+fn solve<T: ToPrimitive, U: ToPrimitive>(a: T, b: U) -> f64 {
     let a_f64 = a.to_f64().unwrap();
     let b_f64 = b.to_f64().unwrap();
 
     (a_f64.powi(2) + b_f64.powi(2)).sqrt()
 }
 
+
 fn main() {
-    // TODO: version manual
+    // NOTE: version manual
     // let a: f32 = 3.0;
     // let b: f64 = 4.0;
 
@@ -38,9 +48,9 @@ fn main() {
 
     // println!("{} ", solve(a_f64, b));
 
-    // TODO: version generic with  num traits
+    // NOTE: version generic with  num traits
 
-    let a: f32 = 3.0;
+    let a: i32 = 3;
     let b: f64 = 4.0;
 
     println!("🟢 result {} ", solve(a, b));

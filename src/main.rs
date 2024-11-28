4314f6dad2041cece3fa4fd33322699703e18f55
voratham.s <voratham.s@linecorp.com>
2024-11-28 14:08:48 +0700

    chore: sove with num_traits or casting type before to pass method

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
+use num_traits::ToPrimitive;
+
 fn solve(a: f64, b: f64) -> f64 {
     (a.powi(2) + b.powi(2)).sqrt()
 }
 
 fn main() {
     let a: f32 = 3.0;
-    let b: f32 = 4.0;
+    let b: f64 = 4.0;
 
-    println!("{} ", solve(a, b));
+    // How to casting manual
+    // let a_f64 = a as f64;
+
+    //  incase import num_traits::ToPrimitive
+    let a_f64 = a.to_f64().unwrap();
+
+    println!("{} ", solve(a_f64, b));
 }
